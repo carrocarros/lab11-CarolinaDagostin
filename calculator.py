6476ffc221bdf58f491c1cd41b82b85988b896a2
carrocarros <carolinadagostin@gmail.com>
2025-11-12 15:05:55 -0500

    modified calculator p1

---
+++ calculator.py
@@ -0,0 +1,24 @@
+import math
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    try:
+        return b / a
+    except ZeroDivisionError:
+        print("Error: Cannot divide by zero.")
+
+def log(a, b):
+    try:
+        return math.log(a, b)
+    except ValueError:
+        print(f"Invalid input.")
+
+def exp(a, b):
+    return a**b
